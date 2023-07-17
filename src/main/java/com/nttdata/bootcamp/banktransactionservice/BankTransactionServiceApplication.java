0ce0ffb2e452052563a9ca555638925591aabbf9
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-07-17 11:32:01 -0500

    primercommit

---
+++ src/main/java/com/nttdata/bootcamp/banktransactionservice/BankTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.banktransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionServiceApplication.class, args);
+	}
+
+}
